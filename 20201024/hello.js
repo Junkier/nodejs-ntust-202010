a743935434701d35c46f4a957309522c8e4e6e73
Junkier <logFM3798@gmail.com>
2020-10-24 10:50:27 +0800

    Console.log

---
+++ 20201024/hello.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+console.log("I'm Jeff.");
+console.log("Today is Saturday!!!");
+
+////////////////////////////////////////////////////////
+
+console.log(1235.16546);
+console.log([2,3,4,5]);  // Array 
+console.log({ "name" : "Jeff" , "age" : 12 });  // Object
+
