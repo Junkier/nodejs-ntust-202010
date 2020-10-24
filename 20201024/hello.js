a98b03b069286cceeb55cfa377ed7283caf72494
Junkier <logFM3798@gmail.com>
2020-10-24 11:02:36 +0800

    Tutorail

--- 20201024/hello.js
+++ 20201024/hello.js
@@ -1,10 +1,14 @@
-console.log("Hello World!");
-console.log("I'm Jeff.");
-console.log("Today is Saturday!!!");
+// console.log("Hello World!");
+// console.log("I'm Jeff.");
+// console.log("Today is Saturday!!!");
+
+// ////////////////////////////////////////////////////////
+
+// console.log(1235.16546);
+// console.log([2,3,4,5]);  // Array 
+// console.log({ "name" : "Jeff" , "age" : 12 });  // Object
 
 ////////////////////////////////////////////////////////
 
-console.log(1235.16546);
-console.log([2,3,4,5]);  // Array 
-console.log({ "name" : "Jeff" , "age" : 12 });  // Object
-
+const express = require("express");
+console.log(express);
