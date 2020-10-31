15a852a6073add7f7306ba430c8d409dd2635046
Junkier <logFM3798@gmail.com>
2020-10-31 16:18:32 +0800

    Tutorial

---
+++ 20201031/router/books.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+let router = express.Router();
+
+
+router.get("/page",(req,res)=>{
+    res.send("This is books page.");
+});
+
+
+router.get("/test",(req,res)=>{
+    res.json({ name : "jeff" , age : 18});
+});
+
+router.get("/hello",(req,res)=>{
+    res.send("Hello QQQQQQ");
+});
+
+module.exports = router;
--- 20201031/tutorial-3.js
+++ 20201031/tutorial-3.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello world!!!");
+
+const booksRouter = require("./router/books.js");
+
+// const booksRouter = router;
+// module.exports   = ;
+
+
+app.get("/abcd",(req,res)=>{
+    res.send("Hello world abcd!!!");
 });
+
+
+app.use("/books",booksRouter);
+
+
 
 let portNum = 8088;
 
 app.listen(portNum , ()=>{
-    console.log("Server is running at http://localhost:"+portNum);
+    console.log("Server is running at http://localhost:" + portNum);
 });
