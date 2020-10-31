d213cb4023a9866c258b7d2efa0ba8b9e2903890
Junkier <logFM3798@gmail.com>
2020-10-31 10:49:17 +0800

    Tutorial

--- 20201031/tutorial-1.js
+++ 20201031/tutorial-1.js
@@ -79,3 +79,34 @@ console.log("result5 :",result5);
 
 
 console.log("Jeff" + "-" + 18);
+
+
+
+console.log("-".repeat(50));
+////////////////////////////////////////////////////////////////
+// 1. 取得 age >= 30 的人名 array 
+//    --> [ "Keven" , "Elle"]
+
+// 2. 取得 scores 總和 >= 250 的資料
+//    並將人名 & 年紀合併成一字串 , 成為 array 元素後回傳
+//    --> ["Jeff-18","Leo-22","Elle-31"]
+
+
+let arrMapSample = [
+    { name : "Jeff"  , age : 18 , scores : [95,88,100] },
+    { name : "Leo"   , age : 22 , scores : [90,97,98] },
+    { name : "Run"   , age : 25 , scores : [75,68,90] },
+    { name : "Keven" , age : 33 , scores : [77,65,32] },
+    { name : "Jenny" , age : 20 , scores : [63,82,91] },
+    { name : "Elle"  , age : 31 , scores : [100,73,83] },
+];
+
+let example1 = arrMapSample.filter(ele=> ele["age"] >=30)
+                           .map(ele=> ele["name"]);
+
+console.log("example1 :",example1);
+
+let example2 = arrMapSample.filter(ele => ( ele["scores"][0] + ele["scores"][1] + ele["scores"][2] ) >=250 )
+                           .map(ele => ( ele["name"] + "-" + ele["age"] )  );
+
+console.log("example2 :",example2);
