994f09c70db697789a4f48295018ac69c0f76462
Junkier <logFM3798@gmail.com>
2020-10-31 10:26:43 +0800

    Tutorial

--- 20201031/tutorial-1.js
+++ 20201031/tutorial-1.js
@@ -41,3 +41,41 @@ console.log("filter2 :",filter2);
 
 console.log("result1 :",result1);
 
+console.log("-".repeat(50));
+
+
+// 數字放大3倍 , -1 , 再取偶數值
+let step1   = sample1.map(n=> n*3);
+let step2   = step1.map(n=>n-1);
+let result2 = step2.filter(n=> n%2 === 0);
+
+console.log("step1 :",step1);
+console.log("step2 :",step2);
+console.log("result2 :",result2);
+
+let result3 = sample1.map(n=>n*3)
+                     .map(n=>n-1)
+                     .filter(n=>n%2===0);
+
+console.log("result3 :",result3);
+
+////////////////////////////////////////////////////////////////
+let sample2 = [
+    { memNo : 1001 , name : "Jeff"    , money: 1000 },
+    { memNo : 1002 , name : "Leo"     , money: 955 },
+    { memNo : 1003 , name : "Keven"   , money: 800 },
+    { memNo : 1004 , name : "Jessica" , money: 2000 },
+    { memNo : 1005 , name : "Jenny"   , money: 1750 },
+];
+let result4 = sample2.map(element => element["name"]);
+console.log("result4 :",result4);
+
+// money >=1000 的 memNo 組成的 array 
+let result5 = sample2.filter(ele=> ele["money"] >=1000)
+                     .map(ele=>ele["memNo"]);
+
+
+console.log("result5 :",result5);
+
+
+console.log("Jeff" + "-" + 18);
