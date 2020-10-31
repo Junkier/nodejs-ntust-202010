f95283b7b022347fc538239515aff895169be89e
Junkier <logFM3798@gmail.com>
2020-10-31 10:02:32 +0800

    Tutorial

--- 20201031/tutorial-1.js
+++ 20201031/tutorial-1.js
@@ -26,5 +26,17 @@ let test = "age";
 let sample1 = [1,2,3,4,5];
 
 let map1 = sample1.map(n => n*2);
+let map2 = sample1.map(n => n*3 +1);
 
-console.log(map1);
+let filter1 = sample1.filter(n => n>3);
+let filter2 = sample1.filter(n => n**3 >=10 );
+
+let result1 = sample1.map(n=> n**3 >=10);
+
+
+console.log("map1 :",map1);
+console.log("map2 :",map2);
+console.log("filter1 :",filter1);
+console.log("filter2 :",filter2);
+
+console.log("result1 :",result1);
