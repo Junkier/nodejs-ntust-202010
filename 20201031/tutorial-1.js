08e39722c09dd4fb4370121652189e42070bd7f3
Junkier <logFM3798@gmail.com>
2020-10-31 09:45:34 +0800

    Tutorial

---
+++ 20201031/tutorial-1.js
@@ -0,0 +1,24 @@
+let arr1 = [1,2,3,4,5];
+
+let obj1 = {
+    name : "Jeff",
+    age  : 18 , 
+    scores : [95,88,100],
+    // test : "abcd"
+};
+
+let test = "age";
+
+console.log(obj1["name"]);
+console.log(obj1["scores"][1]);
+
+// console.log(obj1[name]);
+console.log(obj1[test]);
+console.log(obj1["name"]);
+
+// console.log(obj1[scores][1]);
+
+console.log(obj1.name);
+console.log(obj1.test);
+console.log(obj1[test]);
+console.log(obj1.scores[1]);
