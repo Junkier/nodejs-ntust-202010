7bb7fdfc1e405b7befdfbfc4f927abfc8c46a9cf
Junkier <logFM3798@gmail.com>
2020-10-31 16:49:12 +0800

    Tutorial

--- 20201031/router/about.js
+++ 20201031/router/about.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path    = require("path");
+const fs      = require("fs");
 
 let router = express.Router();
 
@@ -7,8 +8,20 @@ router.get("/",(req,res)=>{
     res.send("This is about page.");
 });
 
-router.get("/testqq",(req,res)=>{
-    res.sendFile( path.join(__dirname,"sample.jpg") );
-})
+// router.get("/testqq",(req,res)=>{
+    // res.sendFile( path.join(__dirname,"sample.jpg") )
+// })
+
+// /about/data
+router.get("/data",(req,res)=>{
+    fs.readFile("data.json","utf8",(err,data)=>{
+        if(err){
+            res.json({message : "error"});
+        };
+
+        console.log(data);
+        res.send(data);
+    });
+});
 
 module.exports = router;
