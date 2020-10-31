8f88ef51aad15303f0b1b65c68f393759c7d5322
Junkier <logFM3798@gmail.com>
2020-10-31 14:18:00 +0800

    Tutorial

--- 20201031/tutorial-2.js
+++ 20201031/tutorial-2.js
@@ -22,3 +22,36 @@ for(let j=0 ; j < arr2.length ; j++){
     console.log(arr2[j]);
     console.log("hello ~~~");
 };
+
+console.log("-".repeat(50));
+
+for(let k = arr2.length-1 ; k >=0 ; k--){
+    console.log(k,arr2[k]);
+};
+
+console.log("-".repeat(50));
+
+
+for(let j=0 ; j < arr2.length ; j++){
+    console.log(arr2[j]);
+    console.log("第" + j + "次執行 (迭代)");
+    // console.log("hello ~~~");
+};
+
+console.log("-".repeat(50));
+
+////////////////////////////////////////////////////////////////
+let arr3 = [1,2,3,4,5,6,7,8,9,10];
+let arr4 = ["AA","BB","CC","DD","EE"];
+let sum  = 0;
+
+for(let i=0 ; i <arr3.length ; i++){
+    let num = arr3[i];
+    sum = sum + num;
+};
+
+console.log("Sum :",sum);
+
+for(let ele of arr4){
+    console.log(ele);
+};
