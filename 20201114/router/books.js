1f33cf4bc717a525b943409a9c21fedf2c68761e
Junkier <logFM3798@gmail.com>
2020-11-14 11:16:10 +0800

    Tutorial

--- 20201114/router/books.js
+++ 20201114/router/books.js
@@ -3,14 +3,24 @@ const fs      = require("fs");
 
 let router = express.Router();
 
+
 router.get("/data",(req,res)=>{
 
-    // let data = { 
-    //     "name" : "jeff",
-    //     "age"  : 18,
-    // };
+    // 非同步
 
-    // res.json(data);
+//     let strData = fs.readFile('sample1.json',"utf8",()=>{});
+
+//     console.log(strData);    // 您會得到神秘的 undefined 
+
+//     let objData = JSON.parse(strData);
+//     res.json(objData);
+
+    console.log("Start!!!");
+
+    let data = {
+        name : "Jeff",
+        age  : 18
+    };
 
     
     fs.readFile("sample1.json","utf8",(err,strData)=>{  // callback function
@@ -19,16 +29,36 @@ router.get("/data",(req,res)=>{
             console.log(err);
         };
 
-        console.log(strData);
+        // console.log(strData);
 
         // Object / JSON / dict 
+        // let objData = JSON.parse(strData);
+
         let objData = JSON.parse(strData);
 
-        // let objData = JSON.parse(strData);
-        res.json(objData);
-  
+        data = JSON.parse(strData);
+
+        console.log(data);
+
+
+        console.log("In callback!!!");
+
+
+        // res.json(data);
+
+        // res.json(objData);
+
+
+        // res.json(strData);
+
 
     });
+
+    res.json(data);
+
+    console.log("Outside callback!!!");
+
+    console.log("End !!!");
 
     
 });
