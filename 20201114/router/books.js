2ce814444e4b0cbc205b59ae3dedee9f60258acf
Junkier <logFM3798@gmail.com>
2020-11-14 10:48:47 +0800

    Tutorial

--- 20201114/router/books.js
+++ 20201114/router/books.js
@@ -1,16 +1,39 @@
 const express = require("express");
+const fs      = require("fs");
 
 let router = express.Router();
 
 router.get("/data",(req,res)=>{
 
-    let data = { 
-        "name" : "jeff",
-        "age"  : 18,
-    };
+    // let data = { 
+    //     "name" : "jeff",
+    //     "age"  : 18,
+    // };
 
-    res.json(data);
+    // res.json(data);
+
+    
+    fs.readFile("sample1.json","utf8",(err,strData)=>{  // callback function
+        if(err){
+            res.json({ message : "* get error"});
+            console.log(err);
+        };
+
+        console.log(strData);
+
+        // Object / JSON / dict 
+        let objData = JSON.parse(strData);
+
+        // let objData = JSON.parse(strData);
+        res.json(objData);
+  
+
+    });
+
+    
 });
+
+
 
 module.exports = router;
 
