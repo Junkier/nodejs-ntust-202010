dabf5e2da0134e2fa997b2089f1c09a5ede2a676
Junkier <logFM3798@gmail.com>
2020-11-21 15:53:41 +0800

    Starting

--- 20201121/router/dramas.js
+++ 20201121/router/dramas.js
@@ -28,18 +28,17 @@ router.get("/getDramaListData",(req,res)=>{
 router.post("/createNewDramaData",(req,res)=>{
 
     let payload = req.body ;
-    console.log(payload);
+
+    // console.log(payload);
     // console.log(payload["name"]);
     // console.log(payload["score"]);
 
 
-    // 1) 不管 dramaId 
+    ////// 1) 不管 dramaId 
 
     // // 讀取 json 
     // let data = fs.readFileSync("./models/sample2.json","utf8");
     // data = JSON.parse(data);
-
-
 
     // // [ {} , {} , {} , ...]
     // // 塞入 array
@@ -50,18 +49,24 @@ router.post("/createNewDramaData",(req,res)=>{
     // fs.writeFileSync("./models/sample2.json",JSON.stringify(data),"utf8");
 
 
-    // 2) 調整 dramaId
+    ////// 2) 調整 dramaId
     // 讀取 json 
     let data = fs.readFileSync("./models/sample2.json","utf8");
     data = JSON.parse(data);
 
     // 取得最新 dramaId 
     // [ "1001" , "1002" , "1003" , ... , "1007" ]
-    let latestDramaId  = data.map(ele => ele["dramaId"]).slice(-1)[0] ;
-    let newDramaId     = Number(latestDramaId) +1 ;
+    // let latestDramaId  = data.map(ele => ele["dramaId"]).slice(-1)[0] ;
+    // let newDramaId     = Number(latestDramaId) + 1 ;
+
+    let latestDramaId  = data.map(ele => Number(ele["dramaId"])) 
+                             .sort((a,b)=> b-a)[0];
+
+    let newDramaId     = latestDramaId + 1 ;
+
     payload["dramaId"] = String( newDramaId );
 
-    
+
 
     // [ {} , {} , {} , ...]
     // 塞入 array
