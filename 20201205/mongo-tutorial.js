8d8c8c8c9e578e7a4371279130b4fdd7c4c3855e
Junkier <logFM3798@gmail.com>
2020-12-05 12:02:52 +0800

    MongoDB

--- 20201205/mongo-tutorial.js
+++ 20201205/mongo-tutorial.js
@@ -3,3 +3,16 @@ db.getCollection('sample1').insertOne({ "name" : "Leo" , "age" : 22 , "message" 
 db.getCollection('sample1').find({})
 
 ////////////////////////////////////
+
+db.getCollection('sample1').insertMany([{
+    "dramaId" : "1001",
+    "category" : "犯罪",
+    "name" : "絕命毒師",
+    "score" : 10.0
+},{...}])
+
+db.getCollection('sample1').insertOne({
+    "name" : "Keven",
+    "age"  : 20 ,
+    "_id"  : 1236
+})
