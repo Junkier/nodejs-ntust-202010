c066abb0b89cb66c6abfe30588a6c4e0d7637e94
Junkier <logFM3798@gmail.com>
2020-12-05 15:04:46 +0800

    Tutorial

--- 20201205/mongo-tutorial-2.js
+++ 20201205/mongo-tutorial-2.js
@@ -30,12 +30,12 @@ let dramaSchema = new mongoose.Schema({
     "score"    : Number,
 },{ collection : "tutorial-2" });
 
-
+// 建立 model
 let dramaModel = conn.model("Drama",dramaSchema);
 
 
   
-// 建立 model
+// 執行 mongoDB find 操作
 dramaModel.find({ "category" : "政治"})
           .then(result=>{
               console.log(result);
@@ -44,15 +44,3 @@ dramaModel.find({ "category" : "政治"})
               console.log(err);
           });
 
-
-
-
-// let dramaSchema = new mongoose.Schema({
-//     // "dramaId" : String,
-//     // "category" : String,
-//     // "name" : String,
-//     // "score" : Number,
-//   }, { collection: "tutorial-2"}
-// );
-
-// let dramaModel = conn.model("Dramas",dramaSchema);
