7aa53ddeb250640949ba4b2daa1b80ef61482f2c
Junkier <logFM3798@gmail.com>
2020-12-05 12:15:20 +0800

    MongoDB

--- 20201205/mongo-tutorial.js
+++ 20201205/mongo-tutorial.js
@@ -1,9 +1,11 @@
+//////
+// 初步練習
 db.getCollection('sample1').insertOne({ "name" : "Leo" , "age" : 22 , "message" : "Hello World!!! "})
 
 db.getCollection('sample1').find({})
 
 ////////////////////////////////////
-
+// insert 
 db.getCollection('sample1').insertMany([{
     "dramaId" : "1001",
     "category" : "犯罪",
@@ -16,3 +18,26 @@ db.getCollection('sample1').insertOne({
     "age"  : 20 ,
     "_id"  : 1236
 })
+
+// find
+
+// category="政治" & name="紙牌屋"
+db.getCollection('sample1').find({ "category" : "政治" ,  "name" : "紙牌屋"  })
+
+// category="政治" & score > 9
+db.getCollection('sample1').find({ 
+    "score"  : {  "$gt" : 9  }  ,
+    "category" : "政治"
+})
+
+// score < 8
+db.getCollection('sample1').find({ 
+    "score"  : {  "$lt" : 8  }  
+})
+
+
+// update 
+
+
+
+// delete
