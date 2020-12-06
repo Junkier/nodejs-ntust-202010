85d4f4d87c90b2c54b8d25389e87074e17676d70
Junkier <logFM3798@gmail.com>
2020-12-06 10:12:32 +0800

    Start

---
+++ 20201206/router/auth.js
@@ -0,0 +1,71 @@
+const express = require("express");
+let router    = express.Router();
+
+const user = {
+    account : "jeff",
+    passwd  : "testqq",
+};
+
+// let isAccountExist = ... ;
+// let isPasswdExist  = ... ;
+// let isUserValid    = ... ;
+// let setUserInfo    = ... ;
+
+let isAccountAndPasswdExist = (req,res,next)=>{
+    if(!req.body.account || !req.body.passwd){
+        res.status(400).json({ message : "帳號或密碼有漏 !"});
+        return;
+    };
+
+    next();
+
+};
+
+let isUserValid = (req,res,next) => {
+    if(req.body.account !== user.account || req.body.passwd !== user.passwd){
+        res.status(400).json({ message : "帳號或密碼有誤!"});
+        return;
+    };
+
+    next();
+
+};
+
+
+let setUserInfo = (req,res,next)=>{
+    console.log("setUserInfo !!!");
+    console.log(req.session);
+
+    req.session.userInfo = {
+        name : "jeff",
+        isLogined : true
+    };
+
+    next();
+};
+
+
+
+// POST /auth
+router.post("/",
+
+    // 1. 檢查 account / passwd 是否存在
+    isAccountAndPasswdExist,
+
+    // 2. 檢查 account / passwd (使用者資訊) 是否有效
+    isUserValid,
+
+    // 3. 設定 user info on session
+    setUserInfo,
+    
+    (req,res)=>{
+        // console.log(req.body);
+        res.json({message:"ok."});
+    }
+);
+
+module.exports = router;
+
+
+
+
---
+++ 20201206/router/dramas.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const fs      = require("fs");
+
+let router    = express.Router();
+
+const validator = require("../utils/validator");
+
+const model   = require("../models");
+
+router.get("/page",(req,res)=>{
+    res.render("dramas.html");
+});
+
+
+// router.use(
+//     validator.isTokenValidInHeaders,
+//     // (req,res,next) => {
+//     //     console.log("router.use middleware 1 !!!");
+//     //     next();
+//     // },
+//     // (req,res,next) => {
+//     //     console.log(req.query);
+//     //     next();
+//     // }
+// );
+
+
+
+
+// GET /dramas/list
+router.get("/list",
+    // validator.isTokenValidInHeaders,
+    (req,res)=>{
+
+        let query =  req.query.type  === "全" ? {} : { "category" : req.query.type };
+
+        model.dramas
+             .find(query)
+             .then(result=>{
+                res.json( { result : result });
+             })
+             .catch(err=>{
+                console.log(err);
+                res.status(500).json({ message : "Server internal fault."});
+             });
+
+    // router.get("/getDramaListData",(req,res)=>{
+
+
+        // let data = fs.readFileSync("./models/sample2.json","utf8");
+        // data = JSON.parse(data);
+
+        // let type = req.query.type;
+
+        // if(type === "全"){
+        //     res.json({ result:data });
+        // }else{
+        //     let filteredData = data.filter(ele => ele["category"] === type);
+        //     res.json({ result : filteredData });
+        // };
+
+    }
+);
+
+
+
+// POST /dramas/detail
+router.post("/detail",(req,res)=>{
+    let payload = req.body;
+
+    model.dramas
+         .create(payload)
+         .then(result=>{
+            res.json({message:"ok." , result : result });
+         })
+         .catch(err=>{
+            console.log(err);
+            res.status(500).json({ message : "Server internal fault."});
+         });
+});
+
+
+
+// POST /dramas/data
+// router.post("/createNewDramaData",(req,res)=>{
+router.post("/data",
+    // validator.isTokenValidInHeaders,
+    (req,res)=>{
+
+
+        let payload = req.body ;
+
+        console.log(payload);
+        // console.log(payload["name"]);
+        // console.log(payload["score"]);
+
+
+        ////// 1) 不管 dramaId 
+
+        // // 讀取 json 
+        // let data = fs.readFileSync("./models/sample2.json","utf8");
+        // data = JSON.parse(data);
+
+        // // [ {} , {} , {} , ...]
+        // // 塞入 array
+        // data.push(payload);
+
+        // // 寫出成 json file 
+        // // fs.writeFileSync("./models/sample2.json",data,"utf8");
+        // fs.writeFileSync("./models/sample2.json",JSON.stringify(data),"utf8");
+
+
+        ////// 2) 調整 dramaId
+        // 讀取 json 
+        let data = fs.readFileSync("./models/sample2.json","utf8");
+        data = JSON.parse(data);
+
+        // 取得最新 dramaId 
+        // [ "1001" , "1002" , "1003" , ... , "1007" ]
+        // let latestDramaId  = data.map(ele => ele["dramaId"]).slice(-1)[0] ;
+        // let newDramaId     = Number(latestDramaId) + 1 ;
+
+        let latestDramaId  = data.map(ele => Number(ele["dramaId"])) 
+                                .sort((a,b)=> b-a)[0];
+
+        let newDramaId     = latestDramaId + 1 ;
+
+        payload["dramaId"] = String( newDramaId );
+
+
+
+        // [ {} , {} , {} , ...]
+        // 塞入 array
+        data.push(payload);
+
+        // 寫出成 json file 
+        // fs.writeFileSync("./models/sample2.json",data,"utf8");
+        fs.writeFileSync("./models/sample2.json",JSON.stringify(data),"utf8");
+
+
+        res.json({message : "ok."});
+
+    }
+);
+
+
+module.exports = router;
